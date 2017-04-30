6cabefbcbbf3f759728f060e0e5a163701e8adb5
Janaka Chathuranga <bjchathuranga@gmail.com>
2017-04-30 21:12:04 +0530

    shows falcon

---
+++ chart.js
@@ -0,0 +1,77 @@
+var myChart;
+
+function initChart(min, max, points)
+{
+    console.log(points);
+    const gap = (max - min)/100;
+    const labels = [];
+    const values = [];
+    for(var i = min; i<=max; i+=gap)
+    {
+        labels.push(i);
+        values.push(0);
+    }
+    for (var i=0; i<points.length;i++)
+    {
+        var point=points[i];
+        //console.log(max);
+        //console.log(point[0]);
+        //console.log(Date.parse(point[0]));
+        //console.log(Date.parse(point[0]));
+        const index = Math.floor((Date.parse(point[0])-min)/gap)
+        values[index]++;
+
+        //break;
+    }
+    console.log(labels);
+    console.log(values);
+    var ctx = document.getElementById("chartCanvas");
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,//.map(function(item){return new Date(item)}),//["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: values,//[12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                //    'rgba(255, 99, 132, 0.2)',
+                //    'rgba(54, 162, 235, 0.2)',
+                //    'rgba(255, 206, 86, 0.2)',
+                //    'rgba(75, 192, 192, 0.2)',
+                //    'rgba(153, 102, 255, 0.2)',
+                //    'rgba(255, 159, 64, 0.2)'
+                //],
+                borderColor: 'rgba(255,99,132,1)',
+                //    'rgba(54, 162, 235, 1)',
+                //    'rgba(255, 206, 86, 1)',
+                //    'rgba(75, 192, 192, 1)',
+                //    'rgba(153, 102, 255, 1)',
+                //    'rgba(255, 159, 64, 1)'
+                //],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive:false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }],
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            'year': 'YYYY',
+                        }}}],
+                barPercentage: 0.05
+            }
+        }
+    });
+}
+
+
+function updateChart(timeRange){
+
+}
--- custom.js
+++ custom.js
@@ -6,34 +6,33 @@ var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-
-var heatLayer = null, whaleStaticLayer=null;
-
+var heatLayer = null
+var drawingPoints = falconOcPoints;
 function setPointsForTime(timeRange){
-    var addressPoints = whalePoints
+    var heatPoints = drawingPoints
         .filter(function(point){
             const t = new Date(point[0]);
             return t>timeRange[0]&&t<timeRange[1];
         })
-        .map(function (p) { return [p[1], p[2],100]; });
+        .map(function (p) {
+            return [p[1], p[2],10000]; });
+    console.log(heatPoints.length);
     if(heatLayer)
     {
         map.removeLayer(heatLayer);
     }
-    heatLayer = L.heatLayer(addressPoints, {
-        radius:10,
-        blur:5
+    heatLayer = L.heatLayer(heatPoints, {
+        radius:6,
+        blur:10
     }).addTo(map);
 
-    //whaleStaticLayer = L.heatLayer(whalePoints.map(function (p) { return [p[1], p[2],100]; })).addTo(map);
+    updateChart(timeRange);
 }
 
-min_date = Date.parse('1929-05-21T02:00Z');
-max_date = Date.parse('2017-01-19T12:16Z');
-
-const gap = 1000*60*60*24*7*4*12;
+min_date =  Date.parse('9/6/1985 8:19 PM'); // Date.parse('1929-12-16 09:00:00.000');
+max_date = Date.parse('11/11/17 11:07 PM');
+const gap = (max_date-min_date)/100//1000*60*60*24*7*4*12;
 const play_gap = gap/2;
-
 var playing = false;
 var currentValue = min_date;
 $( function() {
@@ -75,9 +74,7 @@ function playMethod(){
     {
         pause();
     }
-
-    setTimeout(playMethod, 200);
-
+    setTimeout(playMethod, 50);
 }
-
 playMethod();
+initChart(min_date, max_date, drawingPoints);
